f074a4e0486dca53db029fbfcdd7d8ee90c8f813
Shruti12-png <chouhanshruti44@gmail.com>
2025-07-01 18:34:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from './components/Header';
-import Memecomponent from './components/Memecomponent';
+import Header from './Header';
+import Memecomponent from './Memecomponent';
 
 function App() {
   return (
