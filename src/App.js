ed8556366f1eb6ac96e01ca77a5383401301a5f1
Shruti12-png <chouhanshruti44@gmail.com>
2025-07-01 20:12:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Memecomponent from './components/Memecomponent';
+
+function App() {
+  return (
+    <div >
+      
+        <Header />
+        <Memecomponent />
+        
+      
+    </div>
+  );
+}
+
+export default App;
